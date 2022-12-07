a71c7da10714609d9bf528db8c5edc98f858e051
TheYem <115823916+TheYem@users.noreply.github.com>
2022-12-07 20:28:06 +0530

    Update client.c

--- FTP/client.c
+++ FTP/client.c
@@ -1,94 +1,62 @@
-#include <stdlib.h>
-#include <fcntl.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <time.h>
-#define size 1024
-int main()
-{
-        int sockfd1,connfd1=0,sockfd2,connfd2=0;
-        struct sockaddr_in serv_addr;
-        char buffer[size];
-        char file[100],user[100],pass[100],new[10];
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(4444);
-        serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        sockfd1=socket(AF_INET, SOCK_STREAM, 0);
-        if(sockfd1<0)
-        {
-                printf("Socket creation failed\n");
-                return 1;
-        }
-        connfd1 = connect(sockfd1, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-        if(connfd1<0)
-        {
-                  printf("server accept failed\n");
-              return 1;
-        }
-        else
-                printf("connection established\n");
-        while(1){
-                      printf("\nEnter the username: ");
-                      scanf("%s", user);
-                      send(sockfd1,user,strlen(user)+1,0);
-                      printf("Enter the password: ");
-                      scanf("%s", pass);
-                      send(sockfd1,pass,strlen(pass)+1,0);
-                      recv(sockfd1,buffer,sizeof(buffer),0);
-                      if((strncmp(buffer,"valid",strlen("valid")))==0)
-                      {
-                             printf("valid\n");
-                             break;
-                      }
-                      else
-                        printf("invalid: try once again\n");
-         }
-        serv_addr.sin_port = htons(5555);
-        sockfd2=socket(AF_INET, SOCK_STREAM, 0);
-          if(sockfd2<0)
-          {
-                  printf("Socket creation failed\n");
-                  return 1;
-          }
-  connfd2 = connect(sockfd2, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-          if(connfd2<0)
-          {
-                  printf("server accept failed\n");
-                  return 1;
-          }
-          else
-                  printf("connection established\n");
-         while(1){
-                 printf("\nEnter the filename('exit' to end): ");
-                 scanf("%s", file);
-                 if((strncmp(file,"exit",strlen("exit")))==0)
-                 {
-                         send(sockfd2,"exit",strlen("exit")+1,0);
-                         break;
-                 }
-                send(sockfd2,file,strlen(file)+1,0);
-                 recv(sockfd2,buffer,sizeof(buffer),0);
-                if((strncmp(buffer,"found",strlen("foound")))==0)
-                {
-                        printf("requested file found\n");
-                        printf("enter 'yes' to request file content(or enter 'no'):");
-                        scanf("%s",new);
-                        send(sockfd2,new,strlen(new)+1,0);
-                        if((strncmp(new,"yes",strlen("yes")))==0)
-                        {
-               recv(sockfd2,buffer,sizeof(buffer),0);
-                                printf("content of the file: \n%s",buffer);
-                        }
-                }
-                else
-                        printf("file not found. try again\n");
-         }
-         close(sockfd1);
-         close(sockfd2);
-         return 0;
+#define SIZE 1024
+
+void send_file(FILE *fp, int sockfd){
+  int n;
+  char data[SIZE] = {0};
+
+  while(fgets(data, SIZE, fp) != NULL) {
+    if (send(sockfd, data, sizeof(data), 0) == -1) {
+      perror("[-]Error in sending file.");
+      exit(1);
+    }
+    bzero(data, SIZE);
+  }
+}
+
+int main(){
+  char *ip = "127.0.0.1";
+  int port = 17200;
+  int e;
+
+  int sockfd;
+  struct sockaddr_in server_addr;
+  FILE *fp;
+  char *filename = "send.txt";
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+  printf("[+]Server socket created successfully.\n");
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if(e == -1) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+        printf("[+]Connected to Server.\n");
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("[-]Error in reading file.");
+    exit(1);
+  }
+
+  send_file(fp, sockfd);
+  printf("[+]File data sent successfully.\n");
+
+        printf("[+]Closing the connection.\n");
+  close(sockfd);
+
+  return 0;
 }
